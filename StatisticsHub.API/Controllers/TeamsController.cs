ee056cd4194f63d0903d8c6d8944e9118666dc72
Joe.Chan <chchan0709@gmail.com>
2016-07-10 21:52:34 -0500

    Return appropriate HttpActionResults

--- StatisticsHub.API/Controllers/TeamsController.cs
+++ StatisticsHub.API/Controllers/TeamsController.cs
@@ -5,66 +5,136 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Http.Description;
 using System.Web.Http.OData; //using System.Web.OData; This will cause EnableQuery() to act up, not sure why
 using System.Web.Http.OData.Query;
 using StatisticsHub.API.Models;
 
 namespace StatisticsHub.API.Controllers
 {
-    //allows all clients to call into our API
+    //allow all types of clients to call into our API
     [EnableCorsAttribute("*", "*", "*")]
     public class TeamsController : ApiController
     {
 
         // returns a maximum of 100 rows and limits query options to select, top , filter, and orderby
         [EnableQuery(PageSize=100, AllowedQueryOptions=AllowedQueryOptions.Select | AllowedQueryOptions.Top | AllowedQueryOptions.Filter | AllowedQueryOptions.OrderBy)] 
-        public IQueryable<Team> Get()
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult Get()
         {
-            var teamRepository = new TeamRepository();
-            return teamRepository.Retrieve().AsQueryable();
+            try
+            {
+                var teamRepository = new TeamRepository();
+                return Ok(teamRepository.Retrieve().AsQueryable());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
         }
 
-        public Team Get(int id)
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult Get(int id)
         {
-            Team team = null;
-            var teamRepository = new TeamRepository();
-
-            if (id > 0)
+            try
             {
-                var teams = teamRepository.Retrieve();
-                team = teams.FirstOrDefault(t => t.ID == id);
+                Team team = null;
+                var teamRepository = new TeamRepository();
+
+                if (id > 0)
+                {
+                    var teams = teamRepository.Retrieve();
+                    team = teams.FirstOrDefault(t => t.ID == id);
+
+                    if (team == null)
+                    {
+                        return NotFound();
+                    }
+                }
+
+                return Ok(team);
             }
-            
-            return team;
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
         }
 
 
         //GET api/teams?search=(searchword) or api/teams/(searchword)
-        public IEnumerable<Team> Get(string search)
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult Get(string search)
         {
-            var teamRepository = new TeamRepository();
-            var teams = teamRepository.Retrieve();
-            return teams.Where(t => t.Team_ID.Contains(search));
+            try
+            {
+                var teamRepository = new TeamRepository();
+                var teams = teamRepository.Retrieve();
+
+                var  updatedTeam = teams.Where(t => t.Team_ID.Contains(search)) as Team;
+
+                if (updatedTeam == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(updatedTeam);
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
         }
 
         // POST api/teams
-        public Team Post([FromBody]Team team)
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult Post([FromBody]Team team)
         {
-            var teamRepository = new TeamRepository();
-            var newTeam = teamRepository.Save(team);
-            return newTeam;
+            try
+            {
+                if (team == null) return BadRequest("Team can't be null");
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                var teamRepository = new TeamRepository();
+                var updatedTeam = teamRepository.Save(team) as Team;
+
+                if (updatedTeam == null) return Conflict();
+
+
+                return Created<Team>(Request.RequestUri + updatedTeam.ID.ToString(), updatedTeam);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
         }
 
         // PUT api/teams/5
-        public void Put(int id, [FromBody]Team team)
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult Put(int id, [FromBody]Team team)
         {
-            var teamRepository = new TeamRepository();
-            teamRepository.Save(id, team);
-        }
+            try
+            {
+                if (team == null) return BadRequest("Team can't be null");
+                if (!ModelState.IsValid) return BadRequest(ModelState);
 
-        // DELETE api/teams/5
-        public void Delete(int id)
-        {
+                var teamRepository = new TeamRepository();
+                var updatedTeam = teamRepository.Save(id, team) as Team;
+
+                if (updatedTeam == null) return NotFound();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
         }
     }
 }
--- StatisticsHub.API/Models/Team.cs
+++ StatisticsHub.API/Models/Team.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace StatisticsHub.API.Models
 {
@@ -11,13 +12,21 @@ namespace StatisticsHub.API.Models
         public string Team_ID { get; set; }
         public string Abbreviation { get; set; }
         public bool Active{ get; set; }
+        [Required]
         public string First_Name { get; set; }
+        [Required]
         public string Last_Name { get; set; }
+        [Required]
         public string Conference { get; set; }
+        [Required]
         public string Division { get; set; }
+        [Required]
         public string Site_Name { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Full_Name { get; set; }
     }
 }
